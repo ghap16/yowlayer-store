1e02236f8def7c145f5dd76ff7d90312b904dcb2
Tarek Galal <tare2.galal@gmail.com>
2015-08-31 06:30:09 +0200

    Removed unique contraint for phone number

--- yowsup_ext/layers/store/models/contact.py
+++ yowsup_ext/layers/store/models/contact.py
@@ -2,7 +2,7 @@ from yowsup_ext.layers.store import db
 import peewee
 
 class Contact(db.get_base_model()):
-    number = peewee.CharField(unique=True, null = True)
+    number = peewee.CharField(unique=False, null = False)
     jid = peewee.CharField(null=False, unique=False)
     last_seen_on = peewee.DateTimeField(null=True)
     status = peewee.CharField(null=True)
